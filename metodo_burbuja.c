8459b43bf8f15c16d907d7ec4810ba33c5ce76b7
alejobasti10 <30663591+alejobasti10@users.noreply.github.com>
2017-10-15 13:20:32 -0500

    Add files via upload

---
+++ metodo_burbuja.c
@@ -0,0 +1,30 @@
+/*programa: metodo de ordenaminto burbuja
+elaboro:alejandro gonzalez
+15/10/17*/
+#include <stdio.h>
+
+int main() {
+	int numeros[] = {4,1,2,3,5};
+	int i,aux;
+	int j;
+	// metodo burbuja
+	for (i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			if(numeros[j]> numeros[j+1]){
+				aux=numeros[j];
+				numeros[j]= numeros [j+1];
+				numeros[j+1]=aux ;
+			}
+		}
+	}
+	printf ("\n Orden Ascendente :");
+	for (i=0;i<5;i++){
+		printf("\n%d",numeros[i]);
+	}
+	printf ("\n Orden descendente :");
+	for (i=4;i>=0;i--){
+		printf("\n%d",numeros[i]);
+	}	
+	return 0;
+}
+
